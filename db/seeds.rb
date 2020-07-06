b91d9fb7783eef19d4c37c9d842f0928703702f8
Hope <hopegiometti@gmail.com>
2020-07-06 11:47:00 -0400

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,10 @@ Response.destroy_all
 
 #user seeds
 hope = User.create(username: "HopeThePope", email: "hopegiometti@gmail.com")
+bot = User.create(username: "IAmABot", email: "immabot@gmail.org")
+
+#postcard seeds
+postOne = Postcard.create(message: "hello world", photo: "https://www.marylmartin.com/wp-content/uploads/2017/06/0000597158-1.jpg", user: hope)
+
+#response seeds
+resOne = Response.create(body: "This is g-g-great!", postcard: postOne, user: bot)
